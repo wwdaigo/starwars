98b0c368865713ead7195e0864a9003ce0833fa2
Daigo Matsuoka <marcelo.daigo@gmail.com>
2018-02-18 12:11:52 +0100

    Root reponse object class

---
+++ api/src/main/java/io/wwdaigo/starwars/api/manager/Root.kt
@@ -0,0 +1,11 @@
+package io.wwdaigo.starwars.api.manager
+
+// TODO any as placeholder
+data class Root(
+        val films: Any,
+        val people: Any,
+        val planets: Any,
+        val species: Any,
+        val starships: Any,
+        val vehicles: Any
+)
